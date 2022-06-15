58378237910983c84eac0ae5179bfe0b2b0eef91
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-15 20:55:12 +0300

    implemented question requests

--- src/api/data.js
+++ src/api/data.js
@@ -17,10 +17,13 @@ function createPointer(name, id) {
 
 function addOwner(object) {
     const userId = sessionStorage.getItem('userId');
-    object.owner = createPointer('_User', userId);
+    const result = Object.assign({}, object);
+    result.owner = createPointer('_User', userId);
+    return result;
 }
 
 // Quiz Collection
+
 export async function getQuizes() {
     return await api.get(host + '/classes/Quiz');
 }
@@ -30,8 +33,8 @@ export async function getQuizById(id) {
 }
 
 export async function createQuiz(quiz) {
-    addOwner(quiz);
-    return await api.post(host + '/classes/Quiz', quiz);
+    const body = addOwner(quiz);
+    return await api.post(host + '/classes/Quiz', body);
 }
 
 export async function updateQuiz(id, quiz) {
@@ -42,3 +45,24 @@ export async function deleteQuiz(id) {
     return await api.del(host + '/classes/Quiz/' + id);
 }
 
+// Question Collection
+
+export async function getQuestionsByQuizId(quizId) {
+    const query = JSON.stringify({ quiz: createPointer('Quiz', quizId) });
+    const reponse = await api.get(host + '/classes/Question?where=' + encodeURIComponent(query));
+    return reponse.results;
+}
+
+export async function creareQuestion(quizId, question) {
+    const body = addOwner(question);
+    body.quiz = createPointer('Quiz', quizId);
+    return await api.post(host + '/classes/Question', body);
+}
+
+export async function updateQuestion(id, question) {
+    return api.put(host + '/classes/Question/' + id, question);
+}
+
+export async function deleteQuestion(id) {
+    return api.del(host + '/classes/Question/' + id);
+}
