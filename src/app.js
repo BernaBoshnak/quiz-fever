7bfbc0597832c067c898205d0bacea6565800f37
Berna Boshnak <100863957+BernaBoshnak@users.noreply.github.com>
2022-06-19 20:01:19 +0300

    added quiz template

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import { page, render } from './lib.js';
 
-import { logout as apiLogout } from './api/data.js';
+import { logout as apiLogout, getQuizById, getQuestionsByQuizId } from './api/data.js';
 import { editorPage } from './views/editor/editor.js';
 import { browsePage } from './views/browse.js';
 import { loginPage, registerPage } from './views/auth.js';
+import { quizPage } from './views/quiz/quiz.js';
+import { cube } from './views/common/loader.js';
 
+const cache = {};
 const main = document.getElementById('content');
 setUserNav();
 document.getElementById('logoutBtn').addEventListener('click', logout);
@@ -12,10 +15,25 @@ document.getElementById('logoutBtn').addEventListener('click', logout);
 page('/browse', decorateContext, browsePage);
 page('/login', decorateContext, loginPage);
 page('/register', decorateContext, registerPage);
+page('/quiz/:id', decorateContext, getQuiz, quizPage);
 page('/create', decorateContext, editorPage);
 page('/edit/:id', decorateContext, editorPage);
 
 page.start();
+
+async function getQuiz(ctx, next) {
+    const quizId = ctx.params.id;
+
+    if (cache[quizId] == undefined) {
+        ctx.render(cube());
+        cache[quizId] = await getQuizById(quizId);
+        const ownerId = cache[quizId].owner.objectId;
+        cache[quizId].questions = await getQuestionsByQuizId(quizId, ownerId)
+    }
+
+    ctx.quiz = cache[quizId];
+    next();
+}
 
 function decorateContext(ctx, next) {
     ctx.render = (content) => render(content, main);
